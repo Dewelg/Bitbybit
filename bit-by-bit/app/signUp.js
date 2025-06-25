bc62ed2577ac7090646d2caf84dab80a2850fb1b
DewelG <you@example.com>
2025-06-25 12:14:36 -0400

    frontend getting built out

---
+++ bit-by-bit/app/signUp.js
@@ -0,0 +1,19 @@
+'use client'
+
+import { useState } from "react";
+import { supabase } from "@/lib/supabase";
+import { motion } from "framer-motion";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { NavigationMenu } from "@/components/ui/navigation-menu";
+import { Switch } from "@/components/ui/switch";
+
+
+export default function Home() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
